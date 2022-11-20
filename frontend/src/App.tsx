8f099ad98f144d3d27ee167fcd804d9b7db2712d
Gabriel Cassimiro <gabrielcassimirous@gmail.com>
2022-11-20 16:29:32 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import SalesCard from "./Components/SalesCard"
 import Header from "./Components/Header"
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
